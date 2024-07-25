8f49772f7d4258e41f21e470fd6311c9ab58607a
Wilson <netogoro77@gmail.com>
2024-07-25 08:31:40 -0400

    Update scripts.js

--- Fundamentos de JavaScript Assíncrono/assets/js/scripts.js
+++ Fundamentos de JavaScript Assíncrono/assets/js/scripts.js
@@ -1,22 +1,21 @@
-const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
-const catBtn = document.getElementById('change-cat');
-const catImg = document.getElementById('cat');
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
 
 const getCats = async () => {
-    try{
-        const data = await fetch(BASE_URL);
-    const json = await data.json();
-
-    return json.webpur1;
-    }
-    catch(e){
-        console.log(e.message);
-    }
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+		return json.webpurl;
+	} catch (e) {
+		console.log(e.message);
+	}
 };
 
 const loadImg = async () => {
-    catImg.src = await getCats();
-}
-catBtn.addEventListener('click'. loadImg);
+	const img = document.getElementsByTagName('img')[0];
+	img.src = await getCats();
+};
 
 loadImg();
+
+const btn = document.getElementById('change-cat');
+btn.addEventListener('click', loadImg);
